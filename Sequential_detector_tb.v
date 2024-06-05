80d55138c00c9804dda00bdddc0df3ff7578f7fd
Ashish Ranjan <81768209+ashish5659@users.noreply.github.com>
2024-06-05 20:35:35 +0530

    Update Sequential_detector_tb.v

--- Sequential_detector_tb.v
+++ Sequential_detector_tb.v
@@ -1,16 +1,45 @@
-module Seq_Det_tb;
-  reg in;
-  reg clk;
-  reg rst;
-  wire out;
-  
-  seq_det uut(
-    .out(out),
-    .in(in),
-    .clk(clk),
-    .rst(rst)
-  );
- // clock genaration
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:59:11 06/05/2024
+// Design Name:   seq_det
+// Module Name:   /home/ise/seq_det/seq_det_tb.v
+// Project Name:  seq_det
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: seq_det
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module seq_det_tb;
+
+	// Inputs
+	reg in;
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire out;
+
+	// Instantiate the Unit Under Test (UUT)
+	seq_det uut (
+		.in(in), 
+		.clk(clk), 
+		.rst(rst), 
+		.out(out)
+	);
+
   initial clk =1'b0;
   always #10 clk = ~clk;// toggle clk every 10 time unit
   
@@ -27,44 +56,13 @@ module Seq_Det_tb;
         #20 in = 1;
         #20 in = 1;
         #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-        #20 in = 1;
-        #20 in = 1;
-        #20 in = 0;
-    #50 $finish;
-  end
-  initial begin
-    $monitor("Time: %0d, Reset:%b, Input:%b, Output:%b, Present state:%b",$time, rst,in,out,uut.PS);
-    $dumpfile(dump.vcd);
-    $dumpvars(0, Seq_Det_tb);
-  end
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
 endmodule
-    
-    
-    
+
