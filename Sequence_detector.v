1ff54b91e291f7a4670f37cbbf8ebfbfcd24de52
Ashish Ranjan <81768209+ashish5659@users.noreply.github.com>
2024-06-05 20:36:04 +0530

    Update Sequence_detector.v

--- Sequence_detector.v
+++ Sequence_detector.v
@@ -1,15 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:53:48 06/05/2024 
+// Design Name: 
+// Module Name:    seq_det 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module seq_det(in, clk,rst, out);
   input clk,rst,in;
   output reg out;
   reg[2:0]PS;
   reg[2:0]NS;
   parameter[2:0] s0 = 3'b000,
-   				 s1 = 3'b001,
-  				 s2 = 3'b010, 
-  				 s3 = 3'b011,
-  				 s4 = 3'b100,
-  				 s5 = 3'b101,
-  				 s6 = 3'b110;
+   				  s1 = 3'b001,
+  				     s2 = 3'b010,
+  				     s3 = 3'b011,
+  				     s4 = 3'b100,
+  				     s5 = 3'b101;
   always @(posedge clk or posedge rst)
     begin
       if(rst)
@@ -30,18 +49,8 @@ module seq_det(in, clk,rst, out);
         out = 0;
       end
       s2: begin
-        if(in) NS = s3;
-        else NS = s2;
-        out = 0;
-      end
-      s3: begin
-        if(in) NS = s4;
-        else NS = s0;
-        out = 0;
-      end
-      s3: begin
-        if(in) NS = s4;
-        else NS = s0;
+        if(in) NS = s2;
+        else NS = s3;
         out = 0;
       end
       s3: begin
@@ -55,8 +64,8 @@ module seq_det(in, clk,rst, out);
         out = 0;
       end
       s5: begin
-        if(in) NS = s3;
-        else NS = s2;
+        if(in) NS = s2;
+        else NS = s3;
         out = 1;
       end
       default: begin
@@ -66,6 +75,7 @@ module seq_det(in, clk,rst, out);
     endcase
   end
 endmodule
- 
+
 
         
+
